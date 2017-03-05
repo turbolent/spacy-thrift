083ce8d6817e5cb582e4ebae5a6518a35f49a241
Bastian Müller <bastian@turbolent.com>
2017-03-05 12:46:30 -0800

    update to spaCy 1.6, improve CLI

--- spacyThrift/service.py
+++ spacyThrift/service.py
@@ -1,37 +1,61 @@
+import logging
+import sys
+from typing import List
+
+import click
+import coloredlogs
+import spacy
+from spacy.language import Language
+from spacy.tokens.doc import Doc
+from spacy.tokens.token import Token as SpacyToken
+
 from spacyThrift import SpacyThrift
 from spacyThrift.ttypes import Token
-
-from spacy.en import English
 
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 
-import logging
 
 class Handler:
-    def __init__(self, nlp):
+    def __init__(self, nlp: Language) -> None:
         self.nlp = nlp
 
-    def tag(self, sentence):
-        document = nlp(sentence, parse=False, entity=False)
-        return [Token(element.orth_, element.tag_, element.lemma_)
-                for element in document]
+    @classmethod
+    def _lemma(cls, token: SpacyToken):
+        if token.lemma_ != "-PRON-":
+            return token.lemma_.lower().strip()
+        else:
+            return token.lower_
 
-if __name__ == '__main__':
-    logging.basicConfig()
-    logger = logging.getLogger()
+    def tag(self, sentence: str) -> List[Token]:
+        document = self.nlp(sentence, parse=False, entity=False)   # type: Doc
+        return [
+            Token(element.orth_, element.tag_, self._lemma(element))
+            for element in document   # type: SpacyToken
+        ]
 
-    logger.info("Loading ...")
-    nlp = English(parser=False, tagger=True, entity=False)
 
+@click.command()
+@click.option('--port', default=9090)
+@click.option('--language', default='en')
+def serve(port: int, language: str) -> None:
+    coloredlogs.install(stream=sys.stderr, level=logging.INFO,
+                        fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+    logging.info("Loading ...")
+    nlp = spacy.load(language, parser=False, entity=False,
+                     create_pipeline=lambda nlp: [nlp.tagger])  # type: Language
     handler = Handler(nlp)
     processor = SpacyThrift.Processor(handler)
-    transport = TSocket.TServerSocket(port=9090)
-    tfactory = TTransport.TBufferedTransportFactory()
-    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
-    server = TServer.TThreadedServer(processor, transport, tfactory, pfactory)
-    logger.info("Serving ...")
+    server_socket = TSocket.TServerSocket(port=port)
+    transport_factory = TTransport.TBufferedTransportFactory()
+    protocol_factory = TBinaryProtocol.TBinaryProtocolFactory()
+    server = TServer.TThreadedServer(processor, server_socket, transport_factory, protocol_factory)
+    logging.info("Serving on port %d ...", port)
     server.serve()
+
+
+if __name__ == '__main__':
+    serve()
 
