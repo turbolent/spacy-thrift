d1bf80f1440c5d26d7299f4322db94c5eab77e57
Bastian Müller <bastian@turbolent.com>
2017-03-05 15:29:34 -0800

    move requirements to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='spacy-thrift',
-      version='0.2',
+      version='0.2.1',
       description='spaCy-as-a-service using Thrift',
       keywords='natural language processing',
       url='https://github.com/turbolent/spacy-thrift',
@@ -14,4 +14,9 @@ setup(name='spacy-thrift',
           "Topic :: Text Processing",
       ],
       packages=['spacyThrift'],
-      install_requires=["thrift", "spacy"])
+      install_requires=[
+          "click==6.7",
+          "thrift==0.10.0",
+          "spacy==1.6.0",
+          "coloredlogs==5.2"
+      ])
